ca343348d4448e44f0cef3e4f08a62031bd04003
DJ <dwalker@d3corp.com>
2016-09-13 19:19:24 -0400

    initial commit

---
+++ pokemon.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"math/rand"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+	"unicode/utf8"
+
+	"github.com/codegangsta/negroni"
+)
+
+func getNames(filename string) ([]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lineText := strings.Trim(scanner.Text(), " ")
+		if utf8.RuneCountInString(lineText) > 0 {
+			lines = append(lines, lineText)
+		}
+	}
+	return lines, scanner.Err()
+}
+
+func GenerateName(maxLength int) string {
+	first, _ := getNames("data/first")
+	last, _ := getNames("data/last")
+	either, _ := getNames("data/either")
+
+	allFirst := append(first, either...)
+	allLast := append(last, either...)
+
+	finalFirst := strings.Trim(allFirst[rand.Intn(len(allFirst))], " ")
+	finalLast := strings.Trim(allLast[rand.Intn(len(allLast))], " ")
+
+	finalName := finalFirst + finalLast
+
+	if maxLength > 0 && len(finalName) > maxLength {
+		return GenerateName(maxLength)
+	}
+
+	return finalName
+}
+
+func NameHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(GenerateName(12)))
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	mux := http.NewServeMux()
+	mux.HandleFunc("/generate", NameHandler)
+	n := negroni.Classic()
+	n.UseHandler(mux)
+	n.Run(":8080")
+}
